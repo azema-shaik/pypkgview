bb2dca6a0e92a40441f07f841a103e920b5014b3
azema-shaik <skazema221@gmail.com>
2026-03-02 11:16:44 +0530

    issue with engine.py

---
+++ src/schema.sql
@@ -0,0 +1,53 @@
+CREATE TABLE modules (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+)
+
+CREATE TABLE classes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    module_id INTEGER REFERENCES modules(id),
+    name TEXT,
+    is_descriptor INTEGER,
+    descriptor_type TEXT, -- non_data, data
+    is_nested INTEGER,
+    parent_class TEXT,
+    has_metaclass INTEGER,
+    metaclass_name TEXT,
+)
+
+CREATE TABLE bases (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    class_id INTEGER REFERENCES classes(id),
+    name TEXT
+)
+
+CREATE TABLE functions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    module_id INTEGER REFERENCES modules(id),
+    name TEXT,
+    is_async INTEGER,
+    is_generator INTEGER,
+    has_generator_delegation INTEGER
+)
+
+CREATE TABLE decorators (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    class_id INTEGER REFERENCES classes(id),
+    function_id INTEGER REFERENCES functions(id),
+    name TEXT
+)
+
+CREATE TABLE (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    module_id INTEGER REFERENCES modules(id),
+    name TEXT,
+    type TEXT -- 'constant' | 'variable'
+)
+
+CREATE TABLE imports (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,,
+    module_id INTEGER REFERENCES modules(id),
+    source TEXT,   -- null for direct imports
+    name TEXT,
+    type  TEXT-- 'direct' | 'external' | 'internal_absolute' | 'internal_relative'
+)
