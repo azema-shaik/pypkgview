286c83c53007c0ca991fbd63f566bdc0e5df6e17
azema-shaik <skazema221@gmail.com>
2026-04-07 19:03:58 +0530

    added n_exceptions to classes and methods

--- src/schema.sql
+++ src/schema.sql
@@ -1,57 +1,79 @@
-CREATE TABLE modules (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT
-)
+
+CREATE TABLE modules(
+    id INTEGER PRIMARY KEY,
+    name TEXT);
+
 
 CREATE TABLE classes (
-            id INTEGER PRIMARY KEY,
-            module_id INTEGER REFERENCES modules(id),
-            name TEXT,
-            is_descriptor INTEGER,
-            descriptor_type TEXT, -- non_data, data
-            is_nested INTEGER,
-            is_contextmanager INTEGER,
-            is_iterable INTEGER,
-            is_iterator INTEGER,
-            parent_class TEXT,
-            has_metaclass INTEGER,
-            metaclass TEXT);
+    id INTEGER PRIMARY KEY,
+    module_id INTEGER REFERENCES modules(id),
+    name TEXT,
+    is_descriptor INTEGER,
+    descriptor_type TEXT, -- non_data, data
+    is_nested INTEGER,
+    is_contextmanager INTEGER,
+    is_iterable INTEGER,
+    is_iterator INTEGER,
+    parent_class TEXT,
+    parent_function TEXT,
+    has_metaclass INTEGER,
+    metaclass TEXT);
+
 
 CREATE TABLE bases (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     class_id INTEGER REFERENCES classes(id),
-    name TEXT
-)
+    name TEXT);
+
 
 CREATE TABLE functions (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY ,
     module_id INTEGER REFERENCES modules(id),
     name TEXT,
     is_decorated INTEGER,
     is_async INTEGER,
     is_generator INTEGER,
-    has_generator_delegation INTEGER
-)
+    is_nested INTEGER,
+    has_generator_delegation INTEGER,
+    parent_function TEXT,
+    n_raises INTEGER
+    );
+
+
+    CREATE TABLE methods (
+    id INTEGER PRIMARY KEY ,
+    class_id INTEGER REFERENCES classes(id),
+    name TEXT,
+    is_decorated INTEGER,
+    is_async INTEGER,
+    is_generator INTEGER,
+    is_nested INTEGER,
+    has_generator_delegation INTEGER,
+    parent_function TEXT,
+    n_raises INTEGER);
+
 
 CREATE TABLE decorators (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     class_id INTEGER REFERENCES classes(id),
     function_id INTEGER REFERENCES functions(id),
-    name TEXT
-)
-
-CREATE TABLE (
+    method_id INTEGER REFERENCES methods(id),
+    name TEXT);
+                        
+                     
+CREATE TABLE constants (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     module_id INTEGER REFERENCES modules(id),
     name TEXT,
     type TEXT -- 'constant' | 'variable'
-)
+);
+                        
 
 CREATE TABLE imports (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     module_id INTEGER REFERENCES modules(id),
     source TEXT,   -- null for direct imports
     name TEXT,
-    type  TEXT,-- 'direct' | 'external' | 'internal_absolute' | 'internal_relative'
-    alias TEXT
-)
+    alias TEXT,
+    type  TEXT-- 'direct' | 'external' | 'internal_absolute' | 'internal_relative'
+);
